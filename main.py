8a833b4b34778c4f1fdeb57dbd589dd821603db6
henrikzujeddeloh <henrik.zujeddeloh@protonmail.com>
2023-04-18 15:02:53 +0200

    added: images by camera model bar graph

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ HEIGHT = 5
 TIMEZONE = 'Europe/Berlin'
 
 # file extentions to ignore
-ignore_ext = ('.md', '.MOV', '.mp4', '.CR2', '.RAF')
+ignore_ext = ('.md', '.mov', '.mp4', '.cr2', '.raf')
 
 # returns specified EXIF tag value for image
 def get_field (exif, field):
@@ -34,7 +34,7 @@ def create_df(folder):
     for subdir, dirs, files in os.walk(folder):
         for filename in files:
             # ignores hidden files and files in ignore list
-            if not filename.startswith('.') and not filename.endswith(ignore_ext):
+            if not filename.startswith('.') and not filename.lower().endswith(ignore_ext):
                 f = os.path.join(subdir, filename)
                 if os.path.isfile(f):
                     img = Image.open(f)
@@ -44,17 +44,27 @@ def create_df(folder):
 
                     # if image has EXIF data it saves specified tag values in dataframe
                     if img_exif is None:
-                        print("no EXIF data for " + f)
+                        print("no EXIF data for " + filename)
                     else:
-                        capture_date = get_field(img_exif, 'DateTime')
+                        capture_date = pd.to_datetime(get_field(img_exif, 'DateTime'), format='%Y:%m:%d %H:%M:%S')
                         camera = get_field(img_exif, 'Model')
                         data_frame.loc[i] = [filename, capture_date, camera]
                         i +=1
     return data_frame
 
+
+
+def show_cam(data_frame):
+    data_frame = data_frame.groupby(['camera']).size()
+
+    fig_cam, axs_cam = plt.subplots(figsize=[WIDTH,HEIGHT])
+    data_frame.plot.bar(ax=axs_cam, title="Photos by Camera", rot=0)
+
+
 # instantiates argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument("folder_path", help="path to photos directory which should be scanned")
+parser.add_argument("--cam", help="output number of photos by camera", action="store_true")
 args = parser.parse_args()
 
 # function to open HEIF files
@@ -63,9 +73,12 @@ register_heif_opener()
 # creates pandas DataFrame
 df = create_df(args.folder_path)
 
-
+if args.cam:
+    show_cam(df)
 
 
 
 print(df)
 print(df.info(memory_usage='deep'))
+
+plt.show()
